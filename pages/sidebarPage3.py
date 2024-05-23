ef5f49aef990a78bf936b0146778020666bfe1aa
XaoLa <162096171+XaoLa@users.noreply.github.com>
2024-05-23 10:53:39 +0200

    Delete pages directory

--- pages/sidebarPage3.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title="sidebarPage3", page_icon="📈")
-st.title('Hier könnte ihr Titel stehen')
-st.write('Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.')
