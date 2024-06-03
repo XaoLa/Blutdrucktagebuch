2db190ce22f2915447abd7fb4727be2066b569d3
IzzahS <110169999+IzzahS@users.noreply.github.com>
2024-06-03 21:52:37 +0200

    Update App_Beschreibung.py

--- pages/App_Beschreibung.py
+++ pages/App_Beschreibung.py
@@ -6,7 +6,7 @@ st.write('**Schritt-für-Schritt-Anleitung zur Eingabe von Blutdruckmessungen:**
 st.write('**Login/Register:** Loggen Sie sich mit Ihrem Benutzernamen ein. Falls Sie neu hier sind, erstellen Sie einfach ein neues Konto')
 st.write('**Blutdruckwerte hinzufügen:** Wählen Sie das Datum und die Uhrzeit und tragen Sie dann Ihre Blutdruckwerte ein, sie werden automatisch gespeichert. Nutzen Sie die Buttons „Woche“ und „Monat“, um Ihre Blutdruckwerte über verschiedene Zeiträume hinweg grafisch darzustellen. Diese Visualisierungen helfen Ihnen, Veränderungen und Trends in Ihrem Blutdruck besser zu verstehen.')
 
-multi = '''If you end a line with two spaces,
+multi = '''If you end a line with two spaces,  
 a soft return is used for the next line.
 
 Two (or more) newline characters in a row will result in a hard return.
