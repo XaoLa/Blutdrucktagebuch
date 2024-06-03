0c194a8809984b99af8e3855c30a59f4d11e848c
IzzahS <110169999+IzzahS@users.noreply.github.com>
2024-06-03 22:04:28 +0200

    Update Blutdruck_handhaben.py

--- pages/Blutdruck_handhaben.py
+++ pages/Blutdruck_handhaben.py
@@ -1,10 +1,16 @@
 import streamlit as st
 st.title('Nicht medikamentöse Maßnahmen')
+st.header('Erhörter Blutdruck')
 st.write('**DASH-Diät:** Ernährungsweise mit viel Obst, Gemüse, Vollkornprodukten, mageren Proteinen und fettarmen Milchprodukten.') 
 st.write('**Salzkonsum reduzieren:** Nicht mehr als 5-6 Gramm Salz pro Tag.') 
 st.write('**Regelmäßige körperliche Aktivität Ausdauersport:** Mindestens 30 Minuten moderate Bewegung an den meisten Tagen der Woche (z.B. schnelles Gehen, Joggen, Radfahren).') 
 st.write('**Gewichtskontrolle Gewichtsreduktion:** Erreichen und Halten eines gesunden BMI und Reduzierung des Bauchumfangs.') 
 st.write('**Verzicht auf Rauchen und Alkohol:** Signifikante Verbesserung der allgemeinen Gesundheit und Blutdrucksenkung.') 
 st.write('**Salzkonsum verringern:** Ein geringerer Salzkonsum ist vorteilhaft für die Senkung eines erhöhten Blutdrucks')
-st.write('In Absprache mit dem Arzt kann eine moderate Erhöhung des Salzkonsums helfen, den Blutdruck zu erhöhen. Kleine, häufige Mahlzeiten Vermeidung großer Mahlzeiten: Häufigere kleine Mahlzeiten helfen, den Blutdruck stabil zu halten. Langsame Positionswechsel Langsam aufstehen: Vermeidung plötzlicher Wechsel von einer liegenden oder sitzenden in eine stehende Position, um Schwindel und Blutdruckabfall zu vermeiden. Kreislauftraining und Kompressionsstrümpfe Regelmäßige Bewegung: Moderate Bewegung wie Spazierengehen oder Radfahren hilft, den Kreislauf zu stabilisieren. Kompressionsstrümpfe: Diese können helfen, das Blut zurück zum Herzen zu pumpen und den Blutdruck zu erhöhen.')
-st.write('Maßnahmen zur Erhöhung von niedrigem Blutdruck (Hypotonie) Ausreichende Flüssigkeitszufuhr Viel trinken: Mindestens  2-3 Liter Flüssigkeit pro Tag, vorzugsweise Wasser oder ungesüßter Tee.')
+
+st.header ('Erniegrigter Blutdruck') 
+st.write('**Mahlzeiten:** Häufigere kleine Mahlzeiten helfen, den Blutdruck stabil zu halten.') 
+st.write('**Langsame Positionswechsel:** Vermeidung plötzlicher Wechsel von einer liegenden oder sitzenden in eine stehende Position, um Schwindel und Blutdruckabfall zu vermeiden.')
+st.write('**Kompressionsstrümpfe:** Diese können helfen, das Blut zurück zum Herzen zu pumpen und den Blutdruck zu erhöhen.')
+st.write('**Ausreichende Flüssigkeitszufuhr:** Mindestens  2-3 Liter Flüssigkeit pro Tag, vorzugsweise Wasser oder ungesüßter Tee.')
+st.write('**Salzkonsum erhöhren:** In Absprache mit dem Arzt kann eine moderate Erhöhung des Salzkonsums helfen, den Blutdruck zu erhöhen.')
