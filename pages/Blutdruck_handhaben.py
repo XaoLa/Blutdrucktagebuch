4299a8bd6d54e088eb4a1a167ad4f5f86f620e8f
IzzahS <110169999+IzzahS@users.noreply.github.com>
2024-06-03 22:29:23 +0200

    Update Blutdruck_handhaben.py

--- pages/Blutdruck_handhaben.py
+++ pages/Blutdruck_handhaben.py
@@ -1,21 +1,21 @@
 import streamlit as st
 st.title('Nicht medikamentöse Maßnahmen')
 st.subheader('Erhörter Blutdruck')
-multi = '''**DASH-Diät:** Ernährungsweise mit viel Obst, Gemüse, Vollkornprodukten, mageren Proteinen und fettarmen Milchprodukten.  
-**Salzkonsum reduzieren:** Nicht mehr als 5-6 Gramm Salz pro Tag.  
-**Regelmäßige körperliche Aktivität Ausdauersport:** Mindestens 30 Minuten moderate Bewegung an den meisten Tagen der Woche (z.B. schnelles Gehen, Joggen, Radfahren).  
-**Gewichtskontrolle Gewichtsreduktion:** Erreichen und Halten eines gesunden BMI und Reduzierung des Bauchumfangs.  
-**Verzicht auf Rauchen und Alkohol:** Signifikante Verbesserung der allgemeinen Gesundheit und Blutdrucksenkung.  
-**Salzkonsum verringern:** Ein geringerer Salzkonsum ist vorteilhaft für die Senkung eines erhöhten Blutdrucks
+multi = ''' - **DASH-Diät:** Ernährungsweise mit viel Obst, Gemüse, Vollkornprodukten, mageren Proteinen und fettarmen Milchprodukten.  
+- **Salzkonsum reduzieren:** Nicht mehr als 5-6 Gramm Salz pro Tag.  
+- **Regelmäßige körperliche Aktivität Ausdauersport:** Mindestens 30 Minuten moderate Bewegung an den meisten Tagen der Woche (z.B. schnelles Gehen, Joggen, Radfahren).  
+- **Gewichtskontrolle Gewichtsreduktion:** Erreichen und Halten eines gesunden BMI und Reduzierung des Bauchumfangs.  
+- **Verzicht auf Rauchen und Alkohol:** Signifikante Verbesserung der allgemeinen Gesundheit und Blutdrucksenkung.  
+- **Salzkonsum verringern:** Ein geringerer Salzkonsum ist vorteilhaft für die Senkung eines erhöhten Blutdrucks
 '''
 st.markdown(multi)
 
 
 st.subheader ('Erniegrigter Blutdruck') 
-multi = '''**Mahlzeiten:** Häufigere kleine Mahlzeiten helfen, den Blutdruck stabil zu halten.  
-**Langsame Positionswechsel:** Vermeidung plötzlichlicher Wechsel von einer liegenden oder sitzenden in eine stehende Position, um Schwindel und Blutdruckabfall zu vermeiden.  
-**Kompressionsstrümpfe:** Diese können helfen, das Blut zurück zum Herzen zu pumpen und den Blutdruck zu erhöhen.  
-**Ausreichende Flüssigkeitszufuhr:** Mindestens  2-3 Liter Flüssigkeit pro Tag, vorzugsweise Wasser oder ungesüßter Tee.  
-**Salzkonsum erhöhren:** In Absprache mit dem Arzt kann eine moderate Erhöhung des Salzkonsums helfen, den Blutdruck zu erhöhen.
+multi = '''- **Mahlzeiten:** Häufigere kleine Mahlzeiten helfen, den Blutdruck stabil zu halten.  
+- **Langsame Positionswechsel:** Vermeidung plötzlichlicher Wechsel von einer liegenden oder sitzenden in eine stehende Position, um Schwindel und Blutdruckabfall zu vermeiden.  
+- **Kompressionsstrümpfe:** Diese können helfen, das Blut zurück zum Herzen zu pumpen und den Blutdruck zu erhöhen.  
+- **Ausreichende Flüssigkeitszufuhr:** Mindestens  2-3 Liter Flüssigkeit pro Tag, vorzugsweise Wasser oder ungesüßter Tee.  
+- **Salzkonsum erhöhren:** In Absprache mit dem Arzt kann eine moderate Erhöhung des Salzkonsums helfen, den Blutdruck zu erhöhen.
 '''
 st.markdown(multi)
